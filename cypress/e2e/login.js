1f346b66b697548d7f0cc7a099da3f816e49f725
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-02-21 22:37:47 -0600

    e2e: test login process

---
+++ cypress/e2e/login.js
@@ -0,0 +1,23 @@
+import { buildUser } from '../support/generate';
+
+describe('login', () => {
+  it('should login an existing user', () => {
+    const user = buildUser();
+
+    cy.visit('/');
+    cy.findByText(/register/i).click();
+    cy.findByLabelText(/username/i).type(user.username);
+    cy.findByLabelText(/password/i).type(user.password);
+    cy.findByText(/submit/i).click();
+    cy.findByText(/logout/i).click();
+
+    cy.findByText(/login/i).click();
+    cy.findByLabelText(/username/i).type(user.username);
+    cy.findByLabelText(/password/i).type(user.password);
+    cy.findByText(/submit/i).click();
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`);
+    cy.window().its('localStorage.token').should('be.a', 'string');
+    cy.findByTestId('username-display').should('have.text', user.username);
+  });
+});
