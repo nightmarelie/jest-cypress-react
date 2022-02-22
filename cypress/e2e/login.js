3dce8f5c174244bb55454c134b81b71fb60792f8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-02-21 22:46:56 -0600

    e2e: eliminate duplication in login test

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -3,13 +3,13 @@ import { buildUser } from '../support/generate';
 describe('login', () => {
   it('should login an existing user', () => {
     const user = buildUser();
+    cy.request({
+      url: 'http://localhost:3000/register',
+      method: 'POST',
+      body: user,
+    });
 
     cy.visit('/');
-    cy.findByText(/register/i).click();
-    cy.findByLabelText(/username/i).type(user.username);
-    cy.findByLabelText(/password/i).type(user.password);
-    cy.findByText(/submit/i).click();
-    cy.findByText(/logout/i).click();
 
     cy.findByText(/login/i).click();
     cy.findByLabelText(/username/i).type(user.username);
