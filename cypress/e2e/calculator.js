6771d267b38b6ec4cae7e98c3c4a507ffa9ed4d4
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-02-22 21:23:17 -0600

    refactor: create command

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -11,8 +11,7 @@ describe('anonymous calculator', () => {
 
 describe('authenticated calculator', () => {
   it('displays the username', () => {
-    cy.createUser().then(user => {
-      cy.login(user);
+    cy.loginAsNewUser().then(user => {
       cy.visit('/');
       cy.findByTestId('username-display').should('have.text', user.username);
       cy.findByText(/logout/i).click();
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,5 +25,12 @@ Cypress.Commands.add('login', user => {
     body: user,
   }).then(response => {
     window.localStorage.setItem('token', response.body.user.token);
+    return { ...response.body.user, ...user };
+  });
+});
+
+Cypress.Commands.add('loginAsNewUser', () => {
+  cy.createUser().then(user => {
+    cy.login(user);
   });
 });
